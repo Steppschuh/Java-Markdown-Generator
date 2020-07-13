1543347591a65e08881699da2e33939937ae908f
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2020-07-13 23:41:49 +0300

    Override #build() to automatically cast to OrderedList

--- Source/MarkdownGenerator/src/main/java/net/steppschuh/markdowngenerator/list/OrderedListBuilder.java
+++ Source/MarkdownGenerator/src/main/java/net/steppschuh/markdowngenerator/list/OrderedListBuilder.java
@@ -1,8 +1,19 @@
 package net.steppschuh.markdowngenerator.list;
 
+import net.steppschuh.markdowngenerator.MarkdownBuilder;
+
 public class OrderedListBuilder extends ListBuilder {
 
     @Override protected UnorderedList createMarkdownElement() {
         return new OrderedList();
+    }
+
+    /**
+     * Returns the root {@link MarkdownBuilder#markdownElement}
+     *
+     * @return {@link MarkdownBuilder#markdownElement}
+     */
+    @Override public OrderedList build() {
+        return (OrderedList) super.build();
     }
 }
